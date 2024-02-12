a15c69168380fcdaae33b56a82376a9776825638
Abhiz003 <110383744+Abhiz003@users.noreply.github.com>
2024-02-13 00:11:08 +0530

    Delete JewelleryPROJECT/artgallery/src directory

--- JewelleryPROJECT/artgallery/src/Services/AdminService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export async function adminlogin(creditials){
-    const response =await axios.post(`http://localhost:8080/admin-login`,creditials);
-    return response.data;
-}
